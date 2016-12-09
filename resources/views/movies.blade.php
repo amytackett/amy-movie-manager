a487af3d8d7e197bde4816895b626e7030f592a4
Amy Tackett <tackett.amy@gmail.com>
2016-12-09 18:48:44 +0000

    Adding template and splash page

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,20 @@
+
+@extends('adminlte::page')
+
+@section('title', "Amy's Movie Manager - Movies")
+
+@section('content_header')
+    <h1>Movies</h1>
+@stop
+
+@section('content')
+    <p>Listing of movies will go here.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>@yield('title', config('adminlte.title'))</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -31,14 +31,19 @@
             }
 
             .title {
-                font-size: 96px;
+                font-size: 36px;
+            }
+            
+            img {
+                max-width: 600px;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">@yield('title', config('adminlte.title'))</div>
+                <div><a href="/movies"><img src="https://www.publicdomainpictures.net/pictures/20000/velka/shegoat.jpg#.WEr5OmYT6hw.link" /></a></div>
             </div>
         </div>
     </body>
